615ee11830e107bdd68f95676af66d6ad562549c
mrudoola <47325781+mrudoola@users.noreply.github.com>
2019-09-10 12:06:45 +0530

    Update Dictionary.py

--- Scripting Languages Lab/Python/Dictionary.py
+++ Scripting Languages Lab/Python/Dictionary.py
@@ -1,1 +1,16 @@
 
+dictionary={'is01':'red' ,'is02':'hat','is03':'house','is04':'home','is05':'display','is06':'alpha'}
+dictionary['is08']='pink'
+delete=[]
+print(dictionary)
+for key, value in dictionary.items():
+    print(key ," with value",value)
+    if value[0]=="h":
+        delete.append(key)
+print(delete)
+for i in delete:
+    del dictionary[i]
+val = list(dictionary.values())
+val.sort()
+print(val)
+print(dictionary)
