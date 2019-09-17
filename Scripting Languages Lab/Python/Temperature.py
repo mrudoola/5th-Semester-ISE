e6e329731aad6cc8fcb885d26ccdb9512e73b776
mrudoola <47325781+mrudoola@users.noreply.github.com>
2019-09-17 20:02:30 +0530

    Update Temperature.py

--- Scripting Languages Lab/Python/Temperature.py
+++ Scripting Languages Lab/Python/Temperature.py
@@ -1,1 +1,21 @@
+def celToF():
+	cel = float(input("Enter the temperature in celsius:"))
+	f = (cel * 1.8) + 32
+	print("%0.1f degree celsius is equal to %0.1f degree fahrenheit"%(cel,f))
 
+def celToKelvin():
+	cel = float(input("Enter the temperature in celsius:"))
+	kelvin = (cel + 273)
+	print("%0.1f degree celsius is equal to %0.1f degree kelvin"%(cel,kelvin))
+
+while("true"):
+	print("\nEnter 1 for celsius to fahrenheit conversion\nEnter 2 for celsius to kelvin conversion\nEnter -1 to exit\n");
+	val = input("Enter the choice:")
+	if val == "1": 
+		celToF()
+	elif val == "2":
+		celToKelvin()
+	elif val =="-1":
+		break
+	else:
+		print("Enter a valid choice")
